d7e56fe56254e73d2126a78c83ade0cb419ce186
computador <computador@localhost.localdomain>
2019-10-12 18:47:46 -0300

    Fix the sequelizeconfig, created the user config to create table with sequelize, created the user model

---
+++ src/app/models/User.js
@@ -0,0 +1,15 @@
+import { Model, Sequelize } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      sequelize
+    );
+  }
+}
+export default User;
